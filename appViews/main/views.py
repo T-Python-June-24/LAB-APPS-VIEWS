6b1db76bc99cbd3b091cfa1b2b058d0d54b668ab
9twy <saeed.abdullah.alghamdi@outlook.sa>
2024-07-04 20:38:50 +0300

    solved

---
+++ appViews/main/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+import random
+import string
+def home_view(request:HttpRequest):
+    return HttpResponse("`Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.`")
+def about_view(request:HttpRequest):
+    return HttpResponse("A simple paragraph about Car Rentals.")
+def password_view(request:HttpRequest):
+    upperCaseLetters=string.ascii_uppercase
+    lowerCaseLetters=string.ascii_lowercase
+    symbols=string.punctuation
+    digits=string.digits
+    upper=lower=dig=sym=True
+    all=""
+    if upper:
+        all+=upperCaseLetters
+    if lower:
+        all+=lowerCaseLetters
+    if dig:
+        all+=digits
+    if sym:
+        all+=symbols
+    length=10
+    password="".join(random.sample(all,length))
+
+
+
+    return HttpResponse(password)
+
+
+
+# Create your views here.
