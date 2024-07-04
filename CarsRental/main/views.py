16f68e95f7fbc2fb1c857356fa151f387bdf0ead
naif <naif.n1158@gmail.com>
2024-07-04 17:17:55 +0300

    LAB-APPS-VIEWS Done

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -6,11 +6,9 @@ import string
 
 # Create your views here.
 def index_website(Http_RQ:HttpRequest):
-    msg="<center><h1 style= color:blue>Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.</h1></center>"
-    return HttpResponse(msg)
+    return HttpResponse("<center><h1 style= color:blue>Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.</h1></center>")
 def about(Http_RQ:HttpRequest):
-    msg="<center><h1 style= color:blue>A simple paragraph about Car Rentals. </h1></center>"
-    return HttpResponse(msg)
+    return HttpResponse("<center><h1 style= color:blue>A simple paragraph about Car Rentals. </h1></center>")
 def password_generate(Http_RQ:HttpRequest):
     sentence = string.ascii_letters + string.digits  
     Complete_sentence = ''.join(random.choice(sentence) for _ in range(10))
