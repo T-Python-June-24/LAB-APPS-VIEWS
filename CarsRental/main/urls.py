925bbe11bcffe4ca3b3657d17f8cab7a41049ca1
raynasser <rnff-333@hotmail.com>
2024-07-07 14:14:42 +0300

    DAY18_LAB1

--- CarsRental/main/urls.py
+++ CarsRental/main/urls.py
@@ -3,9 +3,11 @@
 from . import views
 from django.urls import path
 
+app_name = 'main'
+
 
 urlpatterns = [
-    path("", views.home_view),
-    path("about/", views.about_view),
-    path("password/generate/", views.pass_generator)
+    path("", views.home_view, name= 'home'),
+    path("about/", views.about_view, name= 'aboutUs'),
+    path("password/generate/", views.pass_generator, name= 'passGen')
 ]
--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -6,11 +6,13 @@ import random, string
 
 
 def home_view(request:HttpRequest):
-    return HttpResponse("Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.")
+    # return HttpResponse("Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here.")
+    return render(request, 'main/home.html')
 
 
 def about_view(request:HttpRequest):
-    return HttpResponse("A simple paragraph about Car Rentals.")
+    # return HttpResponse("A simple paragraph about Car Rentals.")
+    return render(request, 'main/about.html')
 
 
 def pass_generator(request:HttpRequest):
@@ -19,4 +21,5 @@ def pass_generator(request:HttpRequest):
 
     passGen = ''.join(random.choice(chars) for _ in range(10))
 
-    return HttpResponse(passGen)
+    # return HttpResponse(passGen)
+    return render(request, 'main/pass_generator.html', context= {'pass_generator' : passGen})
