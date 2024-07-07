183958278887218340661aff76a4f7ba3faf966f
bushraali98 <bm-bisho-90210@hotmail.com>
2024-07-07 13:32:35 +0300

    added templates

--- CarsRental/main/urls.py
+++ CarsRental/main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path 
 from . import views
-app_name = "name"
+app_name = "main"
 
 
 urlpatterns = [
-  path("", views.page_view, name="page_view"),
+  path("", views.home_view, name="home_view"),
   path("about/", views.about_view, name="about_view"),
   path("password/generate/", views.password_generator_view, name="password_generate_view")
 ]
--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -4,18 +4,16 @@ import random
 import string
 
 # Create your views here.
-def page_view(request: HttpRequest):
-  content = "Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here."
-  return HttpResponse(content)
+def home_view(request: HttpRequest):
+  return render(request, "main/home.html")
 
 def about_view(request: HttpRequest):
-  content = "A simple paragraph about Car Rentals. "
-  return HttpResponse(content)
+  return render(request, "main/about.html")
 
 def password_generator_view(request: HttpRequest):
   all_characters = string.ascii_letters + string.digits + string.punctuation
   length = 10
   password = ''.join(random.choices(all_characters, k=length))
-  return(HttpResponse(password))
+  return render(request, "main/password_generate.html", {"password": password})
 
 
