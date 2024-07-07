8d624a9343aa2e292f5c235fe806fa5845ed06d1
Abdullah Aldhamen <a.h.aldhamen@hotmail.com>
2024-07-07 23:25:03 +0300

    Edited

--- CarsRental/CarsRental/Cars/urls.py
+++ CarsRental/CarsRental/Cars/urls.py
@@ -4,7 +4,6 @@ from . import views
 Cars = "Cars"
 
 urlpatterns = [
-    path("path/to/view/", views.page_view, name="page_view"),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('password/generate/', views.generate_password, name='generate_password'),
--- CarsRental/CarsRental/Cars/views.py
+++ CarsRental/CarsRental/Cars/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import random
 import string
 
-
-
 def home(request):
-    message = "Hello World, This is my new HOME for Car Rentals Website  ! We're excited to welcome you here."
-    return render(request, 'home.html', {'message': message})
-
-
+  message = "Hello World, This is my new HOME for Car Rentals Website  ! We're excited to welcome you here."
+  return HttpResponse(message)  # Directly return the message
 
 def about(request):
-    about_text = "Hello There, \n welcome to our Car Rentals portal \n It's our pleasure to serve you \n our cars rental range start from 100 SR a day to serve your needs \n If you're looking for a luxerious car we can serve you with that as well"
-    return render(request, 'about.html', {'about_text': about_text})
-
-
+  about_text = " Hello There, \n Thank you for choosing our Cars Rental Portal \n we have plenty of choices that satisfy everyone's needs \n tell us about your expectations to serve you better than you expect"
+  return HttpResponse(about_text)
 
 def generate_password(request):
-    password_length = 10
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for i in range(password_length))
-    return render(request, 'password.html', {'password': password})
+  password_length = 10
+  characters = string.ascii_letters + string.digits + string.punctuation
+  password = ''.join(random.choice(characters) for i in range(password_length))
+  return HttpResponse(password)
