91fb8291a0f81f1f3e68644397e5d7c2ab8b72cc
e7gx <abalgabo@gmail.com>
2024-07-07 14:02:56 +0300

    edit the repo - apply all the req in the lab

--- CarsRental/abdullahApp/urls.py
+++ CarsRental/abdullahApp/urls.py
@@ -6,9 +6,4 @@ urlpatterns = [
     path('', views.homePage, name='home'),
     path('about/', views.aboutPage, name='about'),
     path('password/generate/', views.passwordPage, name='password_generate'),
-    path('langchain/', views.process_csv_data, name='process_csv_data'),
-    path('contact/', views.contactPage, name='contact'),
-    path('chatboot/', views.run_agent, name='run_agent'),
-    path('abdullah/', views.Test, name='abdullah'),
-
 ]
