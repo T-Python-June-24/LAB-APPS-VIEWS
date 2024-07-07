469f8943ff23a45b07ba0300561559cf15a7e62d
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-07 13:57:24 +0300

    Completed Templates / Resolving URLs / Static Files

--- CarsRental/main/views.py
+++ CarsRental/main/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 import string
 import random
+
+
 # Create your views here.
 def home_view(request: HttpRequest):
-    content = "Hello World, This is my new HOME for Car Rentals Website ! we're excited to welcome you here."
-    return HttpResponse(content)
+    return render(request, 'main/home.html')
+
 
 def about_view(request: HttpRequest):
-    content = "A simple paragraph about Car Rentals."
-    return HttpResponse(content)
+    return render(request, 'main/about.html')
+
 
 def generate_password_view(request: HttpRequest):
     password_length = 10
-    content = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=password_length))
-    return HttpResponse(content)
+    password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=password_length))
+    return render(request, 'main/password_generate.html', context={'password': password})
