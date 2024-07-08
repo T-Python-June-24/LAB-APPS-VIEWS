5f929f63b7dc5cb0c71ffdf1de063ce86562e790
Abdullah Aldhamen <a.h.aldhamen@hotmail.com>
2024-07-08 03:47:43 +0300

    All pages are working fine but the link refs are making errors for the home page and can't figure out why

--- CarsRental/CarsRental/Cars/views.py
+++ CarsRental/CarsRental/Cars/views.py
@@ -4,15 +4,18 @@ import random
 import string
 
 def home(request):
-  message = "Hello World, This is my new HOME for Car Rentals Website  ! We're excited to welcome you here."
-  return HttpResponse(message)  # Directly return the message
+
+    return render(request, "Cars/home.html")
 
 def about(request):
-  about_text = " Hello There, \n Thank you for choosing our Cars Rental Portal \n we have plenty of choices that satisfy everyone's needs \n tell us about your expectations to serve you better than you expect"
-  return HttpResponse(about_text)
+  
+    return render(request, "Cars/about.html")
 
 def generate_password(request):
-  password_length = 10
-  characters = string.ascii_letters + string.digits + string.punctuation
-  password = ''.join(random.choice(characters) for i in range(password_length))
-  return HttpResponse(password)
+
+    password_length = 10
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(password_length))
+    
+    return render(request, "Cars/generate_password.html", context= {"password" : password})
